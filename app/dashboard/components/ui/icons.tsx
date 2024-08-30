cb6c8176c875c49aec5bbef431619b7a0cc92c43
AbidHussainMehir <45311025+AbidHussainMehir@users.noreply.github.com>
2024-08-31 00:45:23 +0500

    update debugger text

--- app/dashboard/components/ui/icons.tsx
+++ app/dashboard/components/ui/icons.tsx
@@ -3,8 +3,6 @@ import { useTheme } from 'next-themes'
 
 function IconLogo({ className, ...props }: React.ComponentProps<'svg'>) {
   const { theme } = useTheme()
-
-  // Determine image source based on theme
   const logoSrc = theme === 'dark' ? '/logo-white.svg' : '/logo.svg'
   return (
     <>
@@ -14,18 +12,6 @@ function IconLogo({ className, ...props }: React.ComponentProps<'svg'>) {
         width={'290rem'}
         style={{ marginLeft: '5px' }}
       />
-      {/* // <svg
-    //   fill="currentColor"
-    //   viewBox="0 0 256 256"
-    //   role="img"
-    //   xmlns="http://www.w3.org/2000/svg"
-    //   className={cn('h-4 w-4', className)}
-    //   {...props}
-    // >
-    //   <circle cx="128" cy="128" r="128" fill="black"></circle>
-    //   <circle cx="102" cy="128" r="18" fill="white"></circle>
-    //   <circle cx="154" cy="128" r="18" fill="white"></circle>
-    // </svg> */}
     </>
   )
 }
