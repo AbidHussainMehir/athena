18c58fc838b85fc7f744cc13e4e9955ef207d031
AbidHussainMehir <45311025+AbidHussainMehir@users.noreply.github.com>
2024-09-01 14:20:22 +0500

    fix button and footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -8,6 +8,7 @@ import { useTheme } from 'next-themes'
 
 const Footer: React.FC = () => {
   const theme = useTheme()
+
   const pathname = usePathname()
   const linksClick = (linkText: any) => {
     window._mtm.push({
@@ -32,135 +33,132 @@ const Footer: React.FC = () => {
   }, [])
   return (
     <footer
-      className={`w-[100%] p-1 md:p-2  ${
-        !pathname.includes('search') && !pathname.includes('dashboard')
-          ? 'fixed bottom-0 right-0'
-          : ''
-      }`}
+      className="w-[100%] p-1 md:p-2  pt-2 fixed bottom-0 right-0 "
+      style={{
+        backgroundColor: theme?.theme === 'dark' ? '#0A0A0A' : '#fff'
+      }}
     >
-      {!pathname.includes('search') && !pathname.includes('share') && (
-        <div className="flex flex-wrap mb-4  gap-1 md:gap-4 w-full justify-center">
-          <span
-            className={`${
-              theme?.theme === 'dark' ? 'tag-dark' : 'tag-white'
-            } rounded-lg`}
-          >
-            <Button className="px-1 md:px-2 h-8" variant={'ghost'} size={'md'}>
-              <Link onClick={() => linksClick('dashboard')} href={'/dashboard'}>
-                Dashboard
-              </Link>
-            </Button>
-          </span>
-          <span
-            className={`${
-              theme?.theme === 'dark' ? 'tag-dark' : 'tag-white'
-            } rounded-lg`}
-          >
-            <Button className="px-1 md:px-2 h-8" variant={'ghost'} size={'md'}>
-              <Link
-                onClick={() => linksClick('https://docs.theathena.ai')}
-                href="https://docs.theathena.ai"
-                className="mx-2"
-                target="_blank"
-              >
-                Docs
-              </Link>
-            </Button>
-          </span>
-          <span
-            className={`${
-              theme?.theme === 'dark' ? 'tag-dark' : 'tag-white'
-            } rounded-lg`}
-          >
-            <Button className="px-1 md:px-2 h-8" variant={'ghost'} size={'md'}>
-              <Link
-                onClick={() => linksClick('deck')}
-                href="/deck_doc.pdf"
-                className="mx-2"
-                target="_blank"
-              >
-                Deck
-              </Link>
-            </Button>
-          </span>
-          <span
-            className={`${
-              theme?.theme === 'dark' ? 'tag-dark' : 'tag-white'
-            } rounded-lg`}
-          >
-            <Button
-              className="px-1 md:px-2  w-8 h-8 "
-              variant={'ghost'}
-              size={'icon'}
+      <div className="flex flex-wrap mb-4  gap-1 md:gap-4 w-full justify-center">
+        <span
+          className={`${
+            theme?.theme === 'dark' ? 'tag-dark' : 'tag-white'
+          } rounded-lg`}
+        >
+          <Button className="px-1 md:px-2 h-8" variant={'ghost'} size={'md'}>
+            <Link onClick={() => linksClick('dashboard')} href={'/dashboard'}>
+              Dashboard
+            </Link>
+          </Button>
+        </span>
+        <span
+          className={`${
+            theme?.theme === 'dark' ? 'tag-dark' : 'tag-white'
+          } rounded-lg`}
+        >
+          <Button className="px-1 md:px-2 h-8" variant={'ghost'} size={'md'}>
+            <Link
+              onClick={() => linksClick('https://docs.theathena.ai')}
+              href="https://docs.theathena.ai"
+              className="mx-2"
+              target="_blank"
             >
-              <Link
-                onClick={() => socialLinksClick('discord')}
-                href="https://discord.gg/zRxaseCuGq"
-                target="_blank"
-              >
-                <SiDiscord color="dark" size={18} />
-              </Link>
-            </Button>
-          </span>
-          <span
-            className={`${
-              theme?.theme === 'dark' ? 'tag-dark' : 'tag-white'
-            } rounded-lg`}
-          >
-            <Button
-              className="px-1 md:px-2  w-8 h-8"
-              variant={'ghost'}
-              size={'icon'}
+              Docs
+            </Link>
+          </Button>
+        </span>
+        <span
+          className={`${
+            theme?.theme === 'dark' ? 'tag-dark' : 'tag-white'
+          } rounded-lg`}
+        >
+          <Button className="px-1 md:px-2 h-8" variant={'ghost'} size={'md'}>
+            <Link
+              onClick={() => linksClick('deck')}
+              href="/deck_doc.pdf"
+              className="mx-2"
+              target="_blank"
             >
-              <Link
-                onClick={() => socialLinksClick('twitter')}
-                href="https://twitter.com/theathena"
-                target="_blank"
-              >
-                <SiTwitter size={18} />
-              </Link>
-            </Button>
-          </span>
-          <span
-            className={`${
-              theme?.theme === 'dark' ? 'tag-dark' : 'tag-white'
-            } rounded-lg`}
+              Deck
+            </Link>
+          </Button>
+        </span>
+        <span
+          className={`${
+            theme?.theme === 'dark' ? 'tag-dark' : 'tag-white'
+          } rounded-lg`}
+        >
+          <Button
+            className="px-1 md:px-2  w-8 h-8 "
+            variant={'ghost'}
+            size={'icon'}
           >
-            <Button
-              className="px-1 md:px-2  w-8 h-8"
-              variant={'ghost'}
-              size={'icon'}
+            <Link
+              onClick={() => socialLinksClick('discord')}
+              href="https://discord.gg/zRxaseCuGq"
+              target="_blank"
             >
-              <Link
-                onClick={() => socialLinksClick('telegram')}
-                href="https://twitter.com/theathena"
-                target="_blank"
-              >
-                <SiTelegram size={18} />
-              </Link>
-            </Button>
-          </span>
-          <span
-            className={`${
-              theme?.theme === 'dark' ? 'tag-dark' : 'tag-white'
-            } rounded-lg`}
+              <SiDiscord color="dark" size={18} />
+            </Link>
+          </Button>
+        </span>
+        <span
+          className={`${
+            theme?.theme === 'dark' ? 'tag-dark' : 'tag-white'
+          } rounded-lg`}
+        >
+          <Button
+            className="px-1 md:px-2  w-8 h-8"
+            variant={'ghost'}
+            size={'icon'}
           >
-            <Button
-              className="px-1 md:px-2  w-8 h-8"
-              variant={'ghost'}
-              size={'icon'}
+            <Link
+              onClick={() => socialLinksClick('twitter')}
+              href="https://twitter.com/theathena"
+              target="_blank"
             >
-              <Link
-                onClick={() => socialLinksClick('mail')}
-                href="mailto:hello@theathena.ai"
-                target="_blank"
-              >
-                <SiGmail size={18} />
-              </Link>
-            </Button>
-          </span>
-        </div>
-      )}
+              <SiTwitter size={18} />
+            </Link>
+          </Button>
+        </span>
+        <span
+          className={`${
+            theme?.theme === 'dark' ? 'tag-dark' : 'tag-white'
+          } rounded-lg`}
+        >
+          <Button
+            className="px-1 md:px-2  w-8 h-8"
+            variant={'ghost'}
+            size={'icon'}
+          >
+            <Link
+              onClick={() => socialLinksClick('telegram')}
+              href="https://twitter.com/theathena"
+              target="_blank"
+            >
+              <SiTelegram size={18} />
+            </Link>
+          </Button>
+        </span>
+        <span
+          className={`${
+            theme?.theme === 'dark' ? 'tag-dark' : 'tag-white'
+          } rounded-lg`}
+        >
+          <Button
+            className="px-1 md:px-2  w-8 h-8"
+            variant={'ghost'}
+            size={'icon'}
+          >
+            <Link
+              onClick={() => socialLinksClick('mail')}
+              href="mailto:hello@theathena.ai"
+              target="_blank"
+            >
+              <SiGmail size={18} />
+            </Link>
+          </Button>
+        </span>
+      </div>
     </footer>
   )
 }
