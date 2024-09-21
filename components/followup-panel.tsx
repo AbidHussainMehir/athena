a5d5f93e81b2123323ae7183de9c18ef9325cac6
AbidHussainMehir <45311025+AbidHussainMehir@users.noreply.github.com>
2024-09-21 18:08:18 +0500

    update follow up

--- components/followup-panel.tsx
+++ components/followup-panel.tsx
@@ -7,8 +7,11 @@ import { useActions, useUIState } from 'ai/rsc'
 import type { AI } from '@/app/actions'
 import { UserMessage } from './user-message'
 import { ArrowRight } from 'lucide-react'
+import { useTheme } from 'next-themes'
 
 export function FollowupPanel() {
+  const { theme } = useTheme()
+
   const [input, setInput] = useState('')
   const { submit } = useActions()
   const [, setMessages] = useUIState<typeof AI>()
@@ -44,6 +47,16 @@ export function FollowupPanel() {
         placeholder="Ask a follow-up question..."
         value={input}
         className="pr-14 h-12"
+        style={{
+          boxShadow:
+            theme === 'dark'
+              ? '0 4px 8px 0 rgba(255, 255, 255, 0.2), 0 6px 20px 0 rgba(255, 255, 255, 0.19)'
+              : '0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)',
+          border: theme === 'dark' ? '1px solid #fff' : '1px solid #000',
+          borderRadius: '28px'
+        }}
+        // className="resize-none w-full min-h-12 rounded-fill bg-muted border border-input pl-4 pr-10 pt-3 pb-1 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50'"
+
         onChange={e => setInput(e.target.value)}
       />
       <Button
@@ -51,7 +64,7 @@ export function FollowupPanel() {
         size={'icon'}
         disabled={input.length === 0}
         variant={'ghost'}
-        className="absolute right-1"
+        className="absolute right-3"
       >
         <ArrowRight size={20} />
       </Button>
