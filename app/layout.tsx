5c1f329c58ef7b8114962000bfe51f435612f89e
AbidHussainMehir <45311025+AbidHussainMehir@users.noreply.github.com>
2024-10-03 16:24:07 +0500

    update meta tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -115,7 +115,10 @@ export default function RootLayout({
           name="description"
           content="Athena SearchGPT: Learn to Earn Search Platform."
         />
-        <meta name="keywords" content="TheAthena.ai,TheAthena,searchGPT,Athena SearchGPT,Search Platform,ai,gpt,rewards,AI-powered search engine,AI search engine,Decentralized search engine,Privacy-focused search engine,AI-driven search engine,GPT-powered search engine,Learn and Earn search engine,Web3 search engine,Search engine for crypto users,Search and Earn rewards,Blockchain-based search engine,AI Google alternative,AI search rewards,Secure AI search platform,Privacy-first AI search,Decentralized web search,AI search with data privacy,AI search with tokenomics,Search engine for blockchain users,SearchGPT by Athena,Athena AI-powered search,AI search engine rewards,ChatGPT alternative search,Best AI search engine,Secure AI search engine,AI-powered decentralized search,Tokenized search rewards,Privacy and rewards search engine,AI and Web3 search,Athena AI SearchGPT,theathena.ai search engine,Athena AI Learn and Earn,Athena AI privacy search,Athena AI rewards search,Athena AI vs Google Search,SearchGPT by Athena AI,Athena Learn-to-Earn platform,Athena AI rewards engine,AI-powered Athena search,Athena AI Web3 search platform,Athena search tokenomics,Athena AI blockchain search,Athena AI decentralized search,Athena AI privacy-focused search" />
+        <meta
+          name="keywords"
+          content="TheAthena.ai,TheAthena,searchGPT,Athena SearchGPT,Search Platform,ai,gpt,rewards,AI-powered search engine,AI search engine,Decentralized search engine,Privacy-focused search engine,AI-driven search engine,GPT-powered search engine,Learn and Earn search engine,Web3 search engine,Search engine for crypto users,Search and Earn rewards,Blockchain-based search engine,AI Google alternative,AI search rewards,Secure AI search platform,Privacy-first AI search,Decentralized web search,AI search with data privacy,AI search with tokenomics,Search engine for blockchain users,SearchGPT by Athena,Athena AI-powered search,AI search engine rewards,ChatGPT alternative search,Best AI search engine,Secure AI search engine,AI-powered decentralized search,Tokenized search rewards,Privacy and rewards search engine,AI and Web3 search,Athena AI SearchGPT,theathena.ai search engine,Athena AI Learn and Earn,Athena AI privacy search,Athena AI rewards search,Athena AI vs Google Search,SearchGPT by Athena AI,Athena Learn-to-Earn platform,Athena AI rewards engine,AI-powered Athena search,Athena AI Web3 search platform,Athena search tokenomics,Athena AI blockchain search,Athena AI decentralized search,Athena AI privacy-focused search"
+        />
 
         <meta property="og:title" content="TheAthena.ai" />
         <meta
@@ -125,7 +128,10 @@ export default function RootLayout({
         <meta property="og:image" content="https://theathena.ai/logo.svg" />
         <meta property="og:url" content="https://theathena.ai" />
         <meta property="og:type" content="website" />
-
+        <meta
+          name="google-site-verification"
+          content="TMsJ43oR1s_rX8TcZrBCM538U1hdtB3GbUV5Tzi6_8o"
+        />
         <meta
           name="description"
           content="Athena SearchGPT: Learn to Earn Search Platform."
